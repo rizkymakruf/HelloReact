0607de30a356e7d8ce26e1bcc589683a853b011d
rizkymakruf <armppy8@gmail.com>
2022-11-28 11:41:32 +0700

    initial commit

--- App.js
+++ App.js
@@ -78,7 +78,7 @@ const styles = StyleSheet.create({
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
-    borderRadius: 50,
+    borderRadius: 40,
   },
   title: {
     fontSize: 22,
